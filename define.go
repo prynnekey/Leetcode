f8241635af6f92f967c225fff4e93cee3555bfb0
prynnekey <prynnekey@gmail.com>
2023-01-23 22:31:03 +0800

    新增:链表的遍历

--- define.go
+++ define.go
@@ -1,7 +1,17 @@
 package main
 
+import "fmt"
+
 // 链表
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+// 遍历链表
+func (head *ListNode) traverse() {
+	for head != nil {
+		fmt.Println(head.Val)
+		head = head.Next
+	}
 }
