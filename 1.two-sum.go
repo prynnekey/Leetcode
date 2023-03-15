bb9a4437eed1094caed72465a3ffb9ea3ec451da
prynnekey <prynnekey@gmail.com>
2023-03-15 15:28:48 +0800

    1. 两数之和

--- 1.two-sum.go
+++ 1.two-sum.go
@@ -8,16 +8,19 @@ package main
 
 // @lc code=start
 func twoSum(nums []int, target int) []int {
-	m := make(map[int]int)
+	// 数据 下标
+	hash := make(map[int]int)
 
 	for i, v := range nums {
-		pos, ok := m[target-v]
-		if !ok {
-			m[v] = i
-			continue
+		if index, ok := hash[target-v]; ok {
+			// 存在
+			return []int{index, i}
 		}
-		return []int{pos, i}
+
+		// 不存在
+		hash[v] = i
 	}
+
 	return nil
 }
 
