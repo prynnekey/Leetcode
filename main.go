46c2f9f5cc9898a9b593bc2bb253fe6fcc3c39fb
prynnekey <prynnekey@gmail.com>
2023-04-15 10:32:25 +0800

    test case

--- main.go
+++ main.go
@@ -1,23 +1,10 @@
 package main
 
-import "fmt"
-
-func func1(a, b int) int {
-	x := a + b
-	func2(x)
-	return x + 10086
-}
-
-func func2(x int) {
-	m := x + 1
-	n := x + 7
-	fmt.Printf("m: %v\n", m)
-	fmt.Printf("n: %v\n", n)
-}
+import (
+	"fmt"
+)
 
 func main() {
-	a, b := 2, 5
-	func1(a, b)
-	c := a + b
-	fmt.Printf("c: %v\n", c)
+	a := []int{31, 16, 37, 2, 13, 32, 10, 27, 7, 42, 29, 222, 543, 18, 28, 12, 9}
+	fmt.Printf("a: %v\n", a)
 }
